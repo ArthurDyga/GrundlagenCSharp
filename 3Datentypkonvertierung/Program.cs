380ec35b6576e4dc1fc5f95f43c1bcbaa7b56361
Arthur Leon Dyga <arthurleon.d2@gso.schule.koeln>
2022-09-02 21:25:30 +0200

    Aufgabe 1 bearbeitet und fertig gestellt.

---
+++ 3Datentypkonvertierung/Program.cs
@@ -0,0 +1,7 @@
+﻿//Aufgabe 1
+Console.WriteLine($"Short: MinValue: {short.MinValue} / MaxValue: {short.MaxValue}");
+Console.WriteLine($"Integer: MinValue: {int.MinValue} / MaxValue: {int.MaxValue}");
+Console.WriteLine($"Long: MinValue: {long.MinValue} / MaxValue: {long.MaxValue}");
+Console.WriteLine($"Float: MinValue: {float.MinValue} / MaxValue: {float.MaxValue}");
+Console.WriteLine($"Double: MinValue: {double.MinValue} / MaxValue: {double.MaxValue}");
+Console.WriteLine($"Decimal: MinValue: {decimal.MinValue} / MaxValue: {decimal.MaxValue}");
