315d27365e0729f9fde49d69b38a2b3ca0d3eb9b
Arthur Leon Dyga <arthurleon.d2@gso.schule.koeln>
2022-09-03 15:54:20 +0200

    Aufgaben bearbeitet und fertiggestellt,

--- 3Datentypkonvertierung/Program.cs
+++ 3Datentypkonvertierung/Program.cs
@@ -1,7 +1,45 @@
-﻿//Aufgabe 1
-Console.WriteLine($"Short: MinValue: {short.MinValue} / MaxValue: {short.MaxValue}");
-Console.WriteLine($"Integer: MinValue: {int.MinValue} / MaxValue: {int.MaxValue}");
-Console.WriteLine($"Long: MinValue: {long.MinValue} / MaxValue: {long.MaxValue}");
-Console.WriteLine($"Float: MinValue: {float.MinValue} / MaxValue: {float.MaxValue}");
-Console.WriteLine($"Double: MinValue: {double.MinValue} / MaxValue: {double.MaxValue}");
-Console.WriteLine($"Decimal: MinValue: {decimal.MinValue} / MaxValue: {decimal.MaxValue}");
+﻿//Arbeitsauftrag 1
+Console.WriteLine($"Short: {typeof(short)} hat {sizeof(short)} Bytes / MinValue: {short.MinValue} / MaxValue: {short.MaxValue}");
+Console.WriteLine($"int: {typeof(int)} hat {sizeof(int)} Bytes / {int.MinValue} / MaxValue: {int.MaxValue}");
+Console.WriteLine($"long: {typeof(long)} hat {sizeof(long)} Bytes / MinValue: {long.MinValue} / MaxValue: {long.MaxValue}");
+Console.WriteLine($"float: {typeof(float)} hat {sizeof(float)} Bytes / MinValue: {float.MinValue} / MaxValue: {float.MaxValue}");
+Console.WriteLine($"double: {typeof(double)} hat {sizeof(double)} Bytes / MinValue: {double.MinValue} / MaxValue: {double.MaxValue}");
+Console.WriteLine($"decimal: {typeof(decimal)} hat {sizeof(decimal)} Bytes / MinValue: {decimal.MinValue} / MaxValue: {decimal.MaxValue}");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+//Arbeitsauuftrag 2
+string Vorname = "Arthur";
+int Alter = 15;
+bool Ist_CSharp_Die_Erste_Programmiersprache_Die_sie_Lernen = true;
+Console.WriteLine($"Vorname: {Vorname}");
+Console.WriteLine($"Alter: {Alter}");
+Console.WriteLine($"Ist C# Die erste Programmiersprache die sie lernen?: {Ist_CSharp_Die_Erste_Programmiersprache_Die_sie_Lernen}");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+//Arbeitsauftrag 3
+int DollarZahl = 36;
+char Dollar = (char)DollarZahl;
+Console.WriteLine($"{DollarZahl}{Dollar}");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+//Arbeitsauftrag 4
+Console.WriteLine("Zufällige zahl");
+string Value1 = Console.ReadLine();
+Console.WriteLine("Zufällige Dezimalzahl");
+string Value2 = Console.ReadLine();
+
+short Konvertiert1 = Convert.ToInt16(Value1);
+double Konvertiert2 = Convert.ToDouble(Value2);
+
+Console.WriteLine($"Erfolgreich {Konvertiert1} zu {typeof(short)} konvertiert");
+Console.WriteLine($"Erfolgreich {Konvertiert2} zu {typeof(double)} konvertiert");
+Console.ReadLine();
