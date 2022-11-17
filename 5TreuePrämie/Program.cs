0ba85268ca29c8de77d56f8e19c83e469603c594
Arthur Leon Dyga <arthurleon.d2@gso.schule.koeln>
2022-11-17 10:12:22 +0100

    Aufgabe 5 bearbeitet und fertiggestellt

---
+++ 5TreuePrämie/Program.cs
@@ -0,0 +1,24 @@
+﻿double Prämie;
+Console.WriteLine("Seit wielange arbeiten sie schon?(In jahre)");
+double Jahre = Convert.ToDouble(Console.ReadLine());
+if (Jahre < 10)
+{
+    switch (Jahre)
+    {
+        case < 3:
+            Console.WriteLine($"Keine Prämie");
+            break;
+        case >= 3:
+            Console.WriteLine($"Prämie = 150 Euro pro Jahr");
+            break;
+    }
+}
+if (Jahre > 10)
+{
+    Prämie = ((Jahre - 10) * 10)+150;
+    Console.WriteLine($"Ihre Prämie liegt bei {Prämie:C2} im Jahr");
+}
+else
+{
+
+}
